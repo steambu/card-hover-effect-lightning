bc1d7c8dd4c738df5131c6d08b85def38081ba0d
steambu <123482323+steambu@users.noreply.github.com>
2023-01-24 20:21:22 +0100

    Add files via upload

---
+++ css-hover-effect-lightning/index.js
@@ -0,0 +1,13 @@
+const cards = document.querySelectorAll(".card");
+const wrapper = document.querySelector(".cards");
+
+wrapper.addEventListener("mousemove", function ($event) {
+  cards.forEach((card) => {
+    const rect = card.getBoundingClientRect();
+    const x = $event.clientX - rect.left;
+    const y = $event.clientY - rect.top;
+
+    card.style.setProperty("--xPos", `${x}px`);
+    card.style.setProperty("--yPos", `${y}px`);
+  });
+});
